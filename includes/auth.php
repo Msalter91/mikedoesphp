73e713efc8be29554fcfa7d82523de7c0d9d2ebe
Mike Salter <mikey.l.salter@gmail.com>
2022-04-21 11:18:08 +0100

    remove old includes files

--- includes/auth.php
+++
@@ -1,9 +0,0 @@
-<?php 
-/** 
- * Returns the users authentication status 
- * 
- * @return Boolean True if user is logged in, false is not
-*/
-function isLoggedIn() {
-    return (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in']);
-}
